3e53fad9719debcdf38a347c8201f3f5f8b4c2c7
ShiLiu <liushie@gmail.com>
2015-09-23 20:58:23 +0800

    added Report class; improvements needed

---
+++ OfficeDemo/Report.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Word = Microsoft.Office.Interop.Word;
+
+namespace OfficeDemo
+{
+    
+    class Report
+    {
+        string outputPath;
+
+        public Report(string templatePath, string outputPath, Word._Application app) 
+        {
+            wordApp = app;
+            this.outputPath = outputPath;
+            loadDocFromPath(templatePath);
+        }
+        private Word._Application wordApp { set; get; }
+
+        private Word._Document document { set; get; }
+
+        private void loadDocFromPath(string path) {
+            document = wordApp.Documents.Open(path);
+        }
+
+        public void SetBookmarkNamed(string bookmark, string text) {
+            var place = document.Bookmarks[bookmark];
+            place.Range.Text = text;
+        }
+
+        public void SaveOut() {
+            document.SaveAs2(outputPath);
+        }
+    }
+
+
+}
